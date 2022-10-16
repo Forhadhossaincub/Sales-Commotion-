4cef1322900288df1d3b62ba84a367e21a1c7599
NET <NET@DESKTOP-NSMUMFG>
2022-10-16 16:01:42 +0600

    Add project files.

---
+++ Commission of Product/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commission_of_Product
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.Write("Enter Amount: ");
+
+                decimal sales = decimal.Parse(Console.ReadLine());
+                decimal commission = sales * 6.5M / 100;
+                Console.WriteLine($"Commission  rate is : {commission:0.00}");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+
+            }
+            finally
+            {
+                Console.WriteLine();
+
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
